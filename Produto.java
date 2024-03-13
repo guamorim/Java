1b199b5e8127da7f4faa686011eb5a27ee1bb81a
guamorim <30790555+guamorim@users.noreply.github.com>
2024-03-13 19:06:40 -0300

    Add files via upload

---
+++ Produto.java
@@ -0,0 +1,29 @@
+package Java;
+
+public class Produto {
+    
+String nome;
+Integer peso;
+String preco;
+
+
+public String getNome() {
+    return nome;
+}
+public void setNome(String nome) {
+    this.nome = nome;
+}
+public Integer getPeso() {
+    return peso;
+}
+public void setPeso(Integer peso) {
+    this.peso = peso;
+}
+public String getPreco() {
+    return preco;
+}
+public void setPreco(String preco) {
+    this.preco = preco;
+}
+
+}
